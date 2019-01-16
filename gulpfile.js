bbb4a6aebc3dc7e2e70eaf1a69f762b7b73ea8ed
Isaac Halvorson <hello@hisaac.net>
2019-01-16 17:26:54 -0600

    Fix gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,20 @@
-const { src, dest, watch, series } = require("gulp");
+const { task, src, dest, watch, series } = require("gulp");
 const sass = require("gulp-sass");
 const autoPrefixer = require("gulp-autoprefixer");
 const htmlmin = require("gulp-htmlmin");
 const prettify = require("gulp-jsbeautifier");
 const prettier = require("gulp-prettier");
 
-/// Transpile Sass files to CSS
-function scss() {
+/// Compile Sass
+task("sass", function() {
 	return src("scss/main.scss")
 		.pipe(sass({ outputStyle: "expanded" }))
 		.pipe(autoPrefixer({ browsers: ["last 20 versions"] }))
 		.pipe(dest("themes/web1/source/css"));
-}
+});
 
 /// Prettify HTML
-function clean() {
+task("prettify", function() {
 	return src(["./public**/*.html"])
 		.pipe(htmlmin({
 			collapseWhitespace: true,
@@ -22,10 +22,4 @@ function clean() {
 		}))
 		.pipe(prettify())
 		.pipe(dest("./public"));
-}
-
-watch("scss/**/*", scss);
-
-exports.scss = scss;
-exports.clean = clean;
-exports.default = watch;
+});
