f47bcbf1feae6d7f0c442509a35b7966f6e93eee
Isaac Halvorson <hello@hisaac.net>
2020-07-06 07:09:49 -0500

    Add error handling to extract_hostname filter

--- _plugins/extract_hostname.rb
+++ _plugins/extract_hostname.rb
@@ -4,7 +4,13 @@ module Jekyll
   # Simple filter to extract the hostname from a given URL
   module ExtractHostname
     def extract_hostname(input)
-      URI.parse(input).host.sub(/\Awww\./, '')
+      stripped_input = input.strip
+      raise "The input to 'extact_hostname' was nil or empty" if stripped_input.nil? || stripped_input.empty?
+
+      hostname = URI.parse(stripped_input).host
+      raise "No valid hostname in #{input}" if hostname.nil? || hostname.empty?
+
+      return hostname.sub(/\Awww\./, '')
     end
   end
 end
