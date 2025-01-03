a19dea1e09eac59b2e5fea6511691ac5ca1124bd
Isaac Halvorson <hello@hisaac.net>
2025-01-03 09:13:20 -0600

    Simplify config by moving config files to root (#64)
    * Simplify config by moving config files to root
    
    * Don't format src

--- scripts/format.sh
+++ scripts/format.sh
@@ -4,8 +4,8 @@ source "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")/lib/base.sh"
 trap 'exit_handler "$?" "${0##*/}"' EXIT
 
 function main {
-	mise_exec npx --prefix "${CONFIG_DIR}" \
-		prettier --write . --ignore-path "${CONFIG_DIR}/.prettierignore"
+	cd "${PUBLIC_DIR}" || exit
+	mise_exec npx prettier --write . --config "${PROJECT_ROOT}/.prettierrc.yml"
 }
 
 main "$@"
--- scripts/lint.sh
+++ scripts/lint.sh
@@ -4,8 +4,7 @@ source "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")/lib/base.sh"
 trap 'exit_handler "$?" "${0##*/}"' EXIT
 
 function main {
-	mise_exec npx --prefix "${CONFIG_DIR}" \
-		prettier --write . --ignore-path "${CONFIG_DIR}/.prettierignore"
+	mise_exec npx prettier --check . --config "${PROJECT_ROOT}/.prettierrc.yml"
 }
 
 main "$@"
