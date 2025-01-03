28e2566e522ef1fcb26305e8f281a4631918fd8e
Isaac Halvorson <hello@hisaac.net>
2025-01-03 09:08:36 -0600

    Don't format src

--- scripts/format.sh
+++ scripts/format.sh
@@ -4,7 +4,6 @@ source "$(dirname "$(readlink -f "${BASH_SOURCE[0]}")")/lib/base.sh"
 trap 'exit_handler "$?" "${0##*/}"' EXIT
 
 function main {
-	mise_exec npx prettier --write . --config "${PROJECT_ROOT}/.prettierrc.yml"
 	cd "${PUBLIC_DIR}" || exit
 	mise_exec npx prettier --write . --config "${PROJECT_ROOT}/.prettierrc.yml"
 }
